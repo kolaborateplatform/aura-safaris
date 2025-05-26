e233ba3279d92858d1ed16a1e079bb692a59448a
Okurut Joseph <okurutjoseph97@gmail.com>
2025-05-26 19:56:29 +0300

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,20 @@ import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const _dirname = dirname(_filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig;
